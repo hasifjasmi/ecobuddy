2be6f3618042a56788bf835b4a291c7d54cdb03f
hasifjasmi <hasifj2207@gmail.com>
2023-09-12 00:21:52 +0800

    added base design

--- EcoBuddy/EcoBuddy/ContentView.swift
+++ EcoBuddy/EcoBuddy/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, test muaz!")
+        
+        ZStack {
+            
+            //background
+            VStack {
+                Rectangle()
+                    .foregroundStyle(Color(red: 0.982, green: 0.928, blue: 0.802))
+                    .frame(height: 50)
+                Image("logo_top")
+                    
+                
+                Rectangle()
+                    .foregroundStyle(Color(red: 0.982, green: 0.928, blue: 0.802))
+
+                
+                //bottom navigation
+                ZStack {
+                    Rectangle()
+                        .frame(width: 397,height: 100)
+                        .cornerRadius(25)
+                    .foregroundStyle(Color(red: 0.825, green: 0.64, blue: 0.452))
+                    
+                    HStack (spacing: 20){
+                        Image("home")
+                            .resizable()
+                            .frame(width: 60,height: 60)
+                            .aspectRatio(contentMode: .fit)
+                        Image("maps")
+                            .resizable()
+                            .frame(width: 50,height: 50)
+                            .aspectRatio(contentMode: .fit)
+                        Image("play-button")
+                            .resizable()
+                            .frame(width: 50,height: 50)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        HStack (spacing: 0) {
+                            Image("leaderboard")
+                                .resizable()
+                                .frame(width: 50,height: 50)
+                                .aspectRatio(contentMode: .fit)
+                                
+                            Image("person")
+                                .resizable()
+                                .frame(width: 80,height: 80)
+                            .aspectRatio(contentMode: .fit)
+                        }
+                            
+                    }
+                }
+                    
+            }
+            .ignoresSafeArea()
+            .background(Color(red: 0.982, green: 0.928, blue: 0.802))
         }
-        .padding()
+        
+        
     }
 }
 
