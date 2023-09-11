ce5cff611c713f61b1f795379c70f07d4b0226fd
hasifjasmi <hasifj2207@gmail.com>
2023-09-11 23:00:33 +0800

    started new

---
+++ EcoBuddy/EcoBuddy/EcoBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBuddyApp.swift
+//  EcoBuddy
+//
+//  Created by stdc_15 on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcoBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
