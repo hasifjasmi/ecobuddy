57d3e152ea406f07d019e5bc480092c115859f39
hasifjasmi <hasifj2207@gmail.com>
2023-09-12 09:09:34 +0800

    ADDED FONTS!@!!@!!

--- EcoBuddy/EcoBuddy/login.swift
+++ EcoBuddy/EcoBuddy/login.swift
@@ -23,6 +23,7 @@ struct login: View {
     var body: some View {
         ZStack {
             
+            //Z index = 0
             VStack {
                 //logo
                 Rectangle()
@@ -40,14 +41,73 @@ struct login: View {
                     .foregroundStyle(Color(red: 0.982, green: 0.928, blue: 0.802))
                 
             }
+//            Z index = 1
             //login text
-            Text("Login")
+            VStack {
+                Text("Login")
+                .bold()
+                .font(.custom("Avenir Next Condensed", size: 44))
+                .padding()
+                Rectangle()
+                    .foregroundStyle(Color(red: 0.982, green: 0.928, blue: 0.802))
+                        .frame(height: 100)
+                    
+            }
+            //email password and remember me
+            VStack{
+                Spacer()
+                    .frame(height: 200)
+                Text("Email")
+                    .bold()
+                    .font(.custom("Avenir Next Condensed", size: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                Rectangle()
+                    .foregroundColor(Color.black.opacity(0.5))
+
+                Rectangle()
+                    .frame(width: 300, height: 45)
+                    .foregroundColor(Color.orange.opacity(0.0))
+                    .overlay(RoundedRectangle(cornerRadius: 3).stroke(Color.black, lineWidth: 3))
+                    
+                Text("Paswword")
+                    .bold()
+                    .font(.custom("Avenir Next Condensed", size: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    
+                Text("Forgot Password")
+                    .font(.custom("Avenir Next Condensed", size: 25))
+                
+                HStack{
+                    Rectangle()
+                        .frame(width: 20,height: 20)
+                    Text("Remember me")
+                        .font(.custom("Avenir Next Condensed", size: 25))
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity,alignment: .leading)
+                
+//                button login
+                ZStack {
+                    
+                    RoundedRectangle(cornerRadius: 40)
+                        .frame(width: 280,height: 60)
+                    Text("Login")
+                        .foregroundColor(Color.white)
+                        .font(.custom("HelveticaNeue", size: 25))
+                        .bold()
+                    
+                }
+                
+                Rectangle()
+                    .frame(width:200,height: 2)
+                
+            }.frame(height: 40)
                 
             
-        
-            
                 
-        }
+        }// end of zstack main
         .ignoresSafeArea()
         .background(Color(red: 0.982, green: 0.928, blue: 0.802))
     }
